4ecde53e3f6423a3323e046d7e84cf6f676bf2b9
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-03-22 17:46:18 -0500

    Add files via upload

---
+++ businessm.js
@@ -0,0 +1,22 @@
+{
+	"version": "2.21.3.13 v8.3",
+	"download": "http://anthargo.com/6typ",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-es.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt-BR",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-en.html"
+		}
+	]
+}
